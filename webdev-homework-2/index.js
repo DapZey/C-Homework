a74f187315340adf65c5fd8d2a8aca1f2fa7b7a2
DapZey <72968513+DapZey@users.noreply.github.com>
2024-07-09 17:44:42 +0300

    Update index.js

--- webdev-homework-2/index.js
+++ webdev-homework-2/index.js
@@ -1,1 +1,20 @@
-
+const correctUsername = "admin"
+const correctPassword = "123"
+let personsName;
+function submitButtonClicked(){
+    let usernameText = document.getElementById("usernameInput")
+    let passwordText = document.getElementById("password")
+    document.getElementById("usernameText").style.color = "black"
+    document.getElementById("passwordText").style.color = "black"
+    if (usernameText.value === correctUsername && passwordText.value === correctPassword){
+        personsName = window.prompt("sometext","dude");
+        alert("hello, "+ personsName+"!")
+        return
+    }
+    if (usernameText.value !== correctUsername){
+        document.getElementById("usernameText").style.color = "red"
+    }
+    if (passwordText.value !== correctPassword){
+        document.getElementById("passwordText").style.color = "red"
+    }
+}
